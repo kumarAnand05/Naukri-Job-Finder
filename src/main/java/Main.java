5e672ccdb19749f38e5de10ddd6e8a47005a8524
kumar.anand05@yahoo.com <kumar.anand05@yahoo.com>
2025-07-20 19:18:47 +0530

    Added methods to take user input for job search and get the taken values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import pageObject.Controller;
+
+import io.UserInput;
+
 
 public class Main
 
 {
+    public static void main(String[] args)
+    {
+        
+        // Take user input
+        UserInput userInput = new UserInput();
+        userInput.getUserInput();
+
+        Controller controller = new Controller();
+        controller.startScraping(userInput);
+
+    
+    }
 
 }
